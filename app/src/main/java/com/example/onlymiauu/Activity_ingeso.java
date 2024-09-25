cef472fdc41c7e0d09d425e38c67dcbb91f34c20
mmorenoo <marianjmo06@gmail.com>
2024-09-24 21:31:36 -0300

    Bryan

--- app/src/main/java/com/example/onlymiauu/Activity_ingeso.java
+++ app/src/main/java/com/example/onlymiauu/Activity_ingeso.java
@@ -3,30 +3,56 @@ package com.example.onlymiauu;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import models.Administrador;
 
 public class Activity_ingeso extends AppCompatActivity {
+
+   EditText etUsername, etPassword;
+   Button btnIngresar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ingeso);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        etUsername  = findViewById(R.id.etUsername);
+        etPassword  = findViewById(R.id.etPassword);
+        btnIngresar = findViewById(R.id.btnIngresar);
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String username = etUsername.getText().toString().trim();
+                String password = etPassword.getText().toString().trim();
+
+                Administrador adUser = new Administrador();
+
+                if (username.isEmpty() || password.isEmpty()){
+                        //Toast.makeText(Activity_ingreso.this,"Por favor, ingrese nombre de Usuario y Contraseña", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(Activity_ingeso.this,"Por favor, ingrese nombre de Usuario y Contraseña", Toast.LENGTH_SHORT).show();
+                }else if(adUser.validaLogin(username,password)){
+                    Toast.makeText(Activity_ingeso.this,"Login correcto", Toast.LENGTH_SHORT).show();
+                }else if(!adUser.validaLogin(username,password)){
+                    Toast.makeText(Activity_ingeso.this,"Login incorrecto", Toast.LENGTH_SHORT).show();
+                }
+            }
         });
+
     }
 
     public void irRegistro(View vista){
         Intent miRegistro = new Intent(this, Activity_registro.class);
         startActivity(miRegistro);
     }
+
+
 
 }
