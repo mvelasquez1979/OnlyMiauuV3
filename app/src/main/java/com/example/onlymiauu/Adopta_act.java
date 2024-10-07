4f14f124d02ca55e26ec73f0b51948964d6ac6ab
Angel24U <garridoadamsangel@gmail.com>
2024-10-07 20:23:42 -0300

    Parte de ayuda hecho!

--- app/src/main/java/com/example/onlymiauu/Adopta_act.java
+++ app/src/main/java/com/example/onlymiauu/Adopta_act.java
@@ -15,7 +15,7 @@ public class Adopta_act extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_adopta);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btn_envia), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.gran), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
--- app/src/main/java/com/example/onlymiauu/ayuda_act.java
+++ app/src/main/java/com/example/onlymiauu/ayuda_act.java
@@ -15,7 +15,7 @@ public class ayuda_act extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_ayuda);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.btn_envia), (v, insets) -> {
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.gran), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
