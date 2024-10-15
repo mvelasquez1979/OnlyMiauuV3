05225fc148bcd16fe1621105234273a7d3259a7a
mmorenoo <marianjmo06@gmail.com>
2024-10-15 20:37:24 -0300

    Merge remote-tracking branch 'origin/main'
    # Conflicts:
    #	app/src/main/java/com/example/onlymiauu/Activity_registro.java
    #	app/src/main/java/com/example/onlymiauu/Home_act.java

--- app/src/main/java/com/example/onlymiauu/Activity_ingeso.java
+++ app/src/main/java/com/example/onlymiauu/Activity_ingeso.java
@@ -39,7 +39,6 @@ public class Activity_ingeso extends AppCompatActivity {
                 Administrador adUser = new Administrador();
 
                 if (username.isEmpty() || password.isEmpty()){
-                        //Toast.makeText(Activity_ingreso.this,"Por favor, ingrese nombre de Usuario y Contraseña", Toast.LENGTH_SHORT).show();
                     Toast.makeText(Activity_ingeso.this,"Por favor, ingrese nombre de Usuario y Contraseña", Toast.LENGTH_SHORT).show();
                 }else if(adUser.validaLogin(username,password)){
 
@@ -59,10 +58,5 @@ public class Activity_ingeso extends AppCompatActivity {
         startActivity(miRegistro);
         overridePendingTransition(R.anim.derechaanima, R.anim.izquierdaanima);
     }
-
-    //public void irHome(View vista){
-    //    Intent miHome = new Intent(this, Home_act.class);
-    //    startActivity(miHome);
-    //}
 
 }
--- app/src/main/java/com/example/onlymiauu/Adopta_act.java
+++ app/src/main/java/com/example/onlymiauu/Adopta_act.java
@@ -21,4 +21,34 @@ public class Adopta_act extends AppCompatActivity {
             return insets;
         });
     }
+
+    // Metodo para Evaluar adopciones de gatos
+    public String adoptar(Integer cantidad, Integer edad, Boolean trabaja, Float sueldo) {
+        String criterio = "";
+        switch (cantidad){
+            case 1:
+                if(edad >= 18 && trabaja){
+                    criterio = "Cumple, válido para adoptar 1 gatito";
+                }else{
+                    criterio = "No cumple, vuelve cuando tengas 18 años y/o cuentes con un trabajo";
+                }
+                break;
+            case 2:
+                if(edad >= 18 && trabaja && sueldo >= 600000){
+                    criterio = "Cumple, válido para adoptar 2 gatitos";
+                }else{
+                    criterio = "No cumple, se puede evaluar para adoptar 1 gatito ";
+                }
+                break;
+            default:
+                if(edad >= 18 && trabaja && sueldo >= 1800000){
+                    criterio = "Cumple, válido para adoptar varios gatitos";
+                }else{
+                    criterio = "No cumple, se puede evaluar para adoptar menos gatito ";
+                }
+                break;
+        }
+        return criterio;
+    }
+
 }
